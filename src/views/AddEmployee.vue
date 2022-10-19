8cd9c9c0f3c5068c5a6688366d4857ed80c6fa0e
Dipto K Kundu <dkkundu00@gmail.com>
2022-10-20 00:43:23 +0600

    add new forom view

--- src/views/AddEmployee.vue
+++ src/views/AddEmployee.vue
@@ -2,15 +2,34 @@
   <form class="update-form">
     <div class="form-row">
       <div class="form-group col-md-6">
-        <label for="inputEmail4">Name</label>
-        <input type="text" class="form-control" id="inputEmail4"
-               placeholder="Full Name">
+        <div class="row">
+          <div class="col-md-12">
+            <label for="inputName">Name</label>
+              <input type="text" class="form-control" id="inputName" placeholder="Full Name">
+          </div>
+
+           <div class="col-md-12">
+            <label for="inputPhone">Phone</label>
+              <input type="text" class="form-control" id="inputPhone" placeholder="+88018XXXXXXXX">
+          </div>
+
+
+         <div class="col-md-12">
+            <label for="inputPhoto">Photo</label>
+              <input type="file" class="form-control" id="inputPhoto" @change="onFileChange">
+          </div>
+
+
+        </div>
+
       </div>
-      <div class="form-group col-md-6">
-        <label for="inputPassword4">Phone</label>
-        <input type="text" class="form-control" id="inputPassword4"
-               placeholder="Phone">
+
+      <div id="existing_image" class="form-group col-md-6">
+        <img class="preview-image" id="blah" src="../assets/image/preview-icon.png" alt="preview-page" />
       </div>
+      <div id="preview">
+      <img class="preview-image" v-if="url" :src="url" />
+    </div>
     </div>
     <div class="form-group">
       <label for="inputAddress">Address</label>
@@ -46,11 +65,34 @@
 
 <script>
 export default {
-  name: "AddEmployee"
+  name: "AddEmployee",
+
+  mounted(){
+    this.onFileChange()
+
+  },
+  methods:{
+
+    onFileChange(payload) {
+      //const file = payload.target.files[0]; // use it in case of normal HTML input
+      const file = payload; // in case vuetify file input
+      if (file) {
+        this.imagePreviewURL = URL.createObjectURL(file);
+        URL.revokeObjectURL(file); // free memory
+      } else {
+        this.imagePreviewURL =  null
+      }
+    }
+
+  }
 }
 </script>
 
 <style scoped>
+img#blah {
+    width: 212px;
+    margin-left: 36%;
+}
 
 
 </style>
